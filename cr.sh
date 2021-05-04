5a08dae7e6efa405dcbda49386544143fc5a82f8
Alexandre-Jacques St-Jacques <alexandrovich8@live.ca>
2021-05-04 13:08:56 -0400

    Update cr.sh

--- cr.sh
+++ cr.sh
@@ -254,7 +254,7 @@ release_charts() {
 }
 
 update_index() {
-    local args=(-o "$owner" -r "$repo" -c "$charts_repo_url" --push)
+    local args=(-o "$owner" -r "$repo" --push)
     if [[ -n "$config" ]]; then
         args+=(--config "$config")
     fi
